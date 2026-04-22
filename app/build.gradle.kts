8a4ba73dee778d49893e97452b834c6a8211c317
ddmoney420 <ddmoney420@users.noreply.github.com>
2026-04-21 23:02:42 -0600

    Bump versionCode to 9 for Play Store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.vibrdrome.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
+        versionCode = 9
         versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
